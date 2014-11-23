d45146f1734a1b1ff01f7f97567227cb10088c8f
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2014-11-23 13:10:55 +0100

    BFS full non-recursive implementation

--- tests/breadth_first_search_tester.cpp
+++ tests/breadth_first_search_tester.cpp
@@ -41,5 +41,39 @@ TEST_F(breadth_first_search_tester, tests_empty_graph)
 }
 
 
+/**
+ * Tested graph:
+ * 0 - 1 - 2
+ * |   |   |
+ * 3 - 4 - 5
+ */
+TEST_F(breadth_first_search_tester, test_custom_graph)
+{
+    // given
+    g.add_not_directed_edge(0, 1);
+    g.add_not_directed_edge(0, 3);
+    g.add_not_directed_edge(1, 2);
+    g.add_not_directed_edge(1, 4);
+    g.add_not_directed_edge(2, 5);
+    g.add_not_directed_edge(3, 4);
+    g.add_not_directed_edge(4, 5);
+
+    const int starting_vertex = 0;
+    visitor_mock vm;
+
+    // expect
+    gt::InSequence seq;
+    EXPECT_CALL(vm, visit(0)); // starting_vertex
+    EXPECT_CALL(vm, visit(1));
+    EXPECT_CALL(vm, visit(3));
+    EXPECT_CALL(vm, visit(2));
+    EXPECT_CALL(vm, visit(4));
+    EXPECT_CALL(vm, visit(5));
+
+    // when and then
+    breadth_first_search::run(g, starting_vertex, vm);
+}
+
+
 } // namespace testing
 } // namespace pk
