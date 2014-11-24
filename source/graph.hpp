5b78a25816f4688f2ef1adbdaf64dc37f052b9b2
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2014-11-24 22:29:31 +0100

    Minor refactoring

--- source/graph.hpp
+++ source/graph.hpp
@@ -13,9 +13,10 @@ template<int MAX_NUM_OF_VERTICES, int MAX_VERTEX_DEGREE>
 class graph
 {
 public:
-    typedef pk::vector<int, MAX_VERTEX_DEGREE> adjacency_list;
     static const int max_num_of_vertices = MAX_NUM_OF_VERTICES;
     static const int max_vertex_degree = MAX_VERTEX_DEGREE;
+
+    typedef pk::vector<int, max_vertex_degree> adjacency_list;
 
     void add_directed_edge(const int vertex_id_from, const int vertex_id_to)
     {
@@ -28,14 +29,14 @@ public:
         add_directed_edge(vertex_id_2, vertex_id_1);
     }
 
-    void reset() { for(int i = 0; i < MAX_NUM_OF_VERTICES; ++i) adjacency_lists[i].reset(); }
+    void reset() { for(int i = 0; i < max_num_of_vertices; ++i) adjacency_lists[i].reset(); }
 
-    int size() const { return MAX_NUM_OF_VERTICES; }
+    int size() const { return max_num_of_vertices; }
 
     const adjacency_list& get_adjacency_list(const int vertex_id) const { return adjacency_lists[vertex_id]; }
 
 private:
-    adjacency_list adjacency_lists[MAX_NUM_OF_VERTICES];
+    adjacency_list adjacency_lists[max_num_of_vertices];
 };
 
 
