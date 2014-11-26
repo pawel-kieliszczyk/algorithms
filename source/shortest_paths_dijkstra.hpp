19bdba13b35d0cffc8b9de7e4c44b9b734adc189
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2014-11-26 22:46:02 +0100

    More meaningful names for variables and methods

--- source/shortest_paths_dijkstra.hpp
+++ source/shortest_paths_dijkstra.hpp
@@ -16,8 +16,8 @@ public:
     template<class graph_type, class callback_type>
     static void run(const graph_type& g, const int starting_vertex, callback_type& callback)
     {
-        bool qs[graph_type::max_num_of_vertices];
-        std::fill(qs, qs + graph_type::max_num_of_vertices, false);
+        bool processed_vertices[graph_type::max_num_of_vertices];
+        std::fill(processed_vertices, processed_vertices + graph_type::max_num_of_vertices, false);
 
         int path_lengths[graph_type::max_num_of_vertices];
         std::fill(path_lengths, path_lengths + graph_type::max_num_of_vertices, std::numeric_limits<int>::max());
@@ -31,20 +31,20 @@ public:
             const int u = h.get_root();
             h.remove_root(path_lengths);
 
-            qs[u] = true;
+            processed_vertices[u] = true;
             callback.notify(u, path_lengths[u]);
 
             const typename graph_type::adjacency_list& adj_u = g.get_adjacency_list(u);
             for(int j = 0; j < adj_u.size(); ++j)
             {
                 const int v = adj_u[j].to;
-                if(qs[v])
+                if(processed_vertices[v])
                     continue;
 
                 if(path_lengths[v] > path_lengths[u] + adj_u[j].weight)
                 {
                     path_lengths[v] = path_lengths[u] + adj_u[j].weight;
-                    h.fix_after_change(path_lengths, v);
+                    h.fix_heap_after_path_length_change(path_lengths, v);
                 }
             }
         }
@@ -63,13 +63,13 @@ private:
 
         int get_root() const
         {
-            return h[0];
+            return heap_array[0];
         }
 
-        void remove_root(const int* d)
+        void remove_root(const int* path_lengths)
         {
-            h[0] = h[--size];
-            hp[h[0]] = 0;
+            heap_array[0] = heap_array[--size];
+            heap_array_index[heap_array[0]] = 0;
 
             int parent = 0;
 
@@ -77,46 +77,46 @@ private:
             {
                 const int left = 2 * parent + 1;
                 const int right = left + 1;
-    
+
                 if(left >= size)
                     return;
-    
-                int d_min = d[h[left]];
-                int p_min = left;
+
+                int min_path_length = path_lengths[heap_array[left]];
+                int child_with_min_path_length_index = left;
 
                 if(right < size)
                 {
-                    if(d_min > d[h[right]])
+                    if(min_path_length > path_lengths[heap_array[right]])
                     {
-                        d_min = d[h[right]];
-                        p_min = right;
+                        min_path_length = path_lengths[heap_array[right]];
+                        child_with_min_path_length_index = right;
                     }
                 }
 
-                if(d[h[parent]] <= d_min)
+                if(path_lengths[heap_array[parent]] <= min_path_length)
                     return;
 
-                std::swap(h[parent], h[p_min]);
-                hp[h[parent]] = parent;
-                hp[h[p_min]] = p_min;
+                std::swap(heap_array[parent], heap_array[child_with_min_path_length_index]);
+                heap_array_index[heap_array[parent]] = parent;
+                heap_array_index[heap_array[child_with_min_path_length_index]] = child_with_min_path_length_index;
 
-                parent = p_min;
+                parent = child_with_min_path_length_index;
             }
         }
 
-        void fix_after_change(const int* d, const int v)
+        void fix_heap_after_path_length_change(const int* path_lengths, const int vertex_with_changed_path_length)
         {
-            int child = hp[v];
+            int child = heap_array_index[vertex_with_changed_path_length];
             while(child != 0)
             {
                 int parent = (child - 1) / 2;
 
-                if(d[h[parent]] <= d[h[child]])
+                if(path_lengths[heap_array[parent]] <= path_lengths[heap_array[child]])
                     break;
 
-                std::swap(h[parent], h[child]);
-                hp[h[parent]] = parent;
-                hp[h[child]] = child;
+                std::swap(heap_array[parent], heap_array[child]);
+                heap_array_index[heap_array[parent]] = parent;
+                heap_array_index[heap_array[child]] = child;
 
                 child = parent;
             }
@@ -127,21 +127,21 @@ private:
         {
             for(int i = 0; i < heap_size; ++i)
             {
-                h[i] = i; // probably should be std::numeric_limits<int>::max()
-                hp[i] = i;
+                heap_array[i] = i;
+                heap_array_index[i] = i;
             }
         }
 
         void fix_heap_with_starting_vertex(const int starting_vertex)
         {
-            std::swap(h[0], h[starting_vertex]);
-            hp[starting_vertex] = 0;
-            hp[0] = starting_vertex;
+            std::swap(heap_array[0], heap_array[starting_vertex]);
+            heap_array_index[starting_vertex] = 0;
+            heap_array_index[0] = starting_vertex;
         }
 
-        int size;
-        int h[heap_size];
-        int hp[heap_size];
+        int size; ///< current number of elements in the heap
+        int heap_array[heap_size]; ///< contains vertex ids organized by path lengths
+        int heap_array_index[heap_size]; ///< heap_array_index[u] contains the index of u in heap_array
     };
 };
 
