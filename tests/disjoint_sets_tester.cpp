d393d5b9684e9b72a30484f4705abaa98076dd2a
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2014-12-13 17:10:11 +0100

    Unit tests refactored

--- tests/disjoint_sets_tester.cpp
+++ tests/disjoint_sets_tester.cpp
@@ -17,7 +17,15 @@ struct disjoint_sets_tester : public gt::Test
     static const int MAX_ID = 7;
     typedef pk::disjoint_sets<MAX_ID> disjoint_sets_type;
 
-    disjoint_sets_tester() : no_set_id(disjoint_sets_type::no_set) {}
+    disjoint_sets_tester()
+      : elem_id_1(3),
+        elem_id_2(5),
+        no_set_id(disjoint_sets_type::no_set)
+    {
+    }
+
+    const int elem_id_1;
+    const int elem_id_2;
 
     const int no_set_id;
 
@@ -28,39 +36,29 @@ struct disjoint_sets_tester : public gt::Test
 
 TEST_F(disjoint_sets_tester, tests_element_not_in_set)
 {
-    // given
-    const int elem_id = 3;
-
     // when and then
-    EXPECT_EQ(no_set_id, ds.get_set_id(elem_id));
+    EXPECT_EQ(no_set_id, ds.get_set_id(elem_id_1));
 }
 
 
 TEST_F(disjoint_sets_tester, tests_one_element_in_set)
 {
-    // given
-    const int elem_id = 3;
-
     // when
-    ds.make_set(elem_id);
+    ds.make_set(elem_id_1);
 
     // then
-    EXPECT_NE(no_set_id, ds.get_set_id(elem_id));
+    EXPECT_NE(no_set_id, ds.get_set_id(elem_id_1));
 }
 
 
 TEST_F(disjoint_sets_tester, tests_different_ids_of_two_sets)
 {
-    // given
-    const int elem_id1 = 3;
-    const int elem_id2 = 5;
-
     // when
-    ds.make_set(elem_id1);
-    ds.make_set(elem_id2);
+    ds.make_set(elem_id_1);
+    ds.make_set(elem_id_2);
 
     // then
-    EXPECT_NE(ds.get_set_id(elem_id1), ds.get_set_id(elem_id2));
+    EXPECT_NE(ds.get_set_id(elem_id_1), ds.get_set_id(elem_id_2));
 }
 
 
