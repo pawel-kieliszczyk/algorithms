4863c56270fb59ba985f89e0059e8db4ff2e945c
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2014-12-14 16:23:38 +0100

    Insertions to interval tree

--- source/interval_tree.hpp
+++ source/interval_tree.hpp
@@ -2,17 +2,83 @@
 #define PK_INTERVALTREE_HPP
 
 
+#include <algorithm>
+
+
 namespace pk
 {
+namespace detail
+{
+
+
+template<int N>
+struct counters_size
+{
+    static const int value = 2 * counters_size<N / 2>::value;
+};
+
+
+template<>
+struct counters_size<0>
+{
+    static const int value = 2;
+};
+
+
+} // namespace detail
 
 
 template<int RANGE>
 class interval_tree
 {
 public:
-    int count(const int a, const int b) { return 0; }
+    interval_tree()
+    {
+        std::fill(counters, counters + COUNTERS_SIZE, 0);
+    }
+
+    void insert(const int x)
+    {
+        int v = M + x;
+        ++counters[v];
+
+        while(v != 1)
+        {
+            v /= 2;
+            counters[v] = counters[2*v] + counters[2*v+1];
+        }
+    }
+
+    int count(const int a, const int b)
+    {
+        int va = M + a;
+        int vb = M + b;
+
+        int result = counters[va];
+
+        if(va != vb)
+            result += counters[vb];
+
+        while((va / 2) != (vb / 2))
+        {
+            if(va % 2 == 0)
+                result += counters[va+1];
+
+            if (vb % 2 == 1)
+                result += counters[vb-1];
+
+            va /= 2;
+            vb /= 2;
+        }
+
+        return result;
+    }
+
 private:
-    //
+    static const int COUNTERS_SIZE = detail::counters_size<RANGE-1>::value;
+    static const int M = COUNTERS_SIZE / 2;
+
+    int counters[COUNTERS_SIZE];
 };
 
 
--- tests/interval_tree_tester.cpp
+++ tests/interval_tree_tester.cpp
@@ -14,17 +14,93 @@ namespace testing
 
 struct interval_tree_testing : public gt::Test
 {
-    static const int RANGE = 100;
+    static const int RANGE = 8;
 
     // tested class:
-    pk::interval_tree<RANGE> t; // accepts points from [0, 99]
+    pk::interval_tree<RANGE> t; // accepts points from [0, RANGE-1]
 };
 
 
 TEST_F(interval_tree_testing, tests_empty_tree)
 {
     // when and then
-    EXPECT_EQ(0, t.count(0, 99));
+    EXPECT_EQ(0, t.count(0, 7));
+}
+
+
+TEST_F(interval_tree_testing, tests_one_point_once_in_tree)
+{
+    // given
+    const int x = 3;
+
+    // when
+    t.insert(x);
+
+    // then
+    EXPECT_EQ(1, t.count(0, 7));
+    EXPECT_EQ(0, t.count(0, 2));
+    EXPECT_EQ(1, t.count(3, 3));
+    EXPECT_EQ(0, t.count(4, 7));
+}
+
+
+TEST_F(interval_tree_testing, tests_one_point_twice_in_tree)
+{
+    // given
+    const int x = 3;
+
+    // when
+    t.insert(x);
+    t.insert(x);
+
+    // then
+    EXPECT_EQ(2, t.count(0, 7));
+    EXPECT_EQ(0, t.count(0, 2));
+    EXPECT_EQ(2, t.count(3, 3));
+    EXPECT_EQ(0, t.count(4, 7));
+}
+
+
+TEST_F(interval_tree_testing, tests_two_points_once_in_tree)
+{
+    // given
+    const int x1 = 3;
+    const int x2 = 5;
+
+    // when
+    t.insert(x1);
+    t.insert(x2);
+
+    // then
+    EXPECT_EQ(2, t.count(0, 7));
+    EXPECT_EQ(0, t.count(0, 2));
+    EXPECT_EQ(1, t.count(3, 3));
+    EXPECT_EQ(0, t.count(4, 4));
+    EXPECT_EQ(1, t.count(5, 5));
+    EXPECT_EQ(0, t.count(6, 7));
+
+    EXPECT_EQ(2, t.count(3, 5));
+}
+
+
+TEST_F(interval_tree_testing, tests_two_points_twice_in_tree)
+{
+    // given
+    const int x1 = 0;
+    const int x2 = 7;
+
+    // when
+    t.insert(x1);
+    t.insert(x1);
+
+    t.insert(x2);
+    t.insert(x2);
+
+    // then
+    EXPECT_EQ(4, t.count(0, 7));
+    EXPECT_EQ(2, t.count(0, 0));
+    EXPECT_EQ(0, t.count(1, 6));
+    EXPECT_EQ(2, t.count(7, 7));
 }
 
 
