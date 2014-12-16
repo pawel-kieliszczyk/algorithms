3cb0bb76bfe5bf1d6a97d5b26b3b97140f64d66b
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2014-12-16 15:49:36 +0100

    Additional test for 2-dim interval tree

--- tests/interval_tree_2d_tester.cpp
+++ tests/interval_tree_2d_tester.cpp
@@ -54,5 +54,93 @@ TEST_F(interval_tree_2d_tester, tests_one_point_once_in_tree)
 }
 
 
+TEST_F(interval_tree_2d_tester, tests_one_point_twice_in_tree)
+{
+    // given
+    const entry_type e(3, 2);
+
+    // when
+    t.insert(e);
+    t.insert(e);
+
+    // then
+    EXPECT_EQ(2, t.count(range_type(0, 7), range_type(0, 5)));
+
+    EXPECT_EQ(0, t.count(range_type(0, 2), range_type(0, 5)));
+    EXPECT_EQ(0, t.count(range_type(4, 7), range_type(0, 5)));
+
+    EXPECT_EQ(0, t.count(range_type(0, 7), range_type(0, 1)));
+    EXPECT_EQ(0, t.count(range_type(0, 7), range_type(3, 5)));
+
+    EXPECT_EQ(2, t.count(range_type(3, 3), range_type(2, 2)));
+}
+
+
+TEST_F(interval_tree_2d_tester, tests_two_points_once_in_tree)
+{
+    // given
+    const entry_type e1(3, 2);
+    const entry_type e2(5, 4);
+
+    // when
+    t.insert(e1);
+    t.insert(e2);
+
+    // then
+    EXPECT_EQ(2, t.count(range_type(0, 7), range_type(0, 5)));
+
+    EXPECT_EQ(0, t.count(range_type(0, 2), range_type(0, 5)));
+    EXPECT_EQ(0, t.count(range_type(4, 4), range_type(0, 5)));
+    EXPECT_EQ(0, t.count(range_type(6, 7), range_type(0, 5)));
+
+    EXPECT_EQ(0, t.count(range_type(0, 7), range_type(0, 1)));
+    EXPECT_EQ(0, t.count(range_type(0, 7), range_type(3, 3)));
+    EXPECT_EQ(0, t.count(range_type(0, 7), range_type(5, 5)));
+
+    EXPECT_EQ(1, t.count(range_type(3, 3), range_type(2, 2)));
+    EXPECT_EQ(1, t.count(range_type(5, 5), range_type(4, 4)));
+}
+
+
+TEST_F(interval_tree_2d_tester, tests_two_points_twice_in_tree)
+{
+    // given
+    const entry_type e1(0, 0);
+    const entry_type e2(7, 5);
+
+    // when
+    t.insert(e1);
+    t.insert(e1);
+
+    t.insert(e2);
+    t.insert(e2);
+
+    // then
+    EXPECT_EQ(4, t.count(range_type(0, 7), range_type(0, 5)));
+
+    EXPECT_EQ(0, t.count(range_type(1, 6), range_type(0, 5)));
+    EXPECT_EQ(0, t.count(range_type(0, 7), range_type(1, 4)));
+
+    EXPECT_EQ(2, t.count(range_type(0, 0), range_type(0, 0)));
+    EXPECT_EQ(2, t.count(range_type(7, 7), range_type(5, 5)));
+}
+
+
+TEST_F(interval_tree_2d_tester, tests_all_points_once_in_tree)
+{
+    // when
+    for(int x = 0; x <= 7; ++x)
+        for(int y = 0; y <= 5; ++y)
+            t.insert(entry_type(x, y));
+
+    // then
+    EXPECT_EQ(48, t.count(range_type(0, 7), range_type(0, 5)));
+
+    for(int x = 0; x <= 7; ++x)
+        for(int y = 0; y <= 5; ++y)
+            EXPECT_EQ(1, t.count(range_type(x, x), range_type(y, y)));
+}
+
+
 } // namespace testing
 } // namespace pk
