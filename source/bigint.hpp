11e1bed63ae7b0b2cbed8fce3dca97d1770b9c54
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2014-12-18 18:36:44 +0100

    Equality and inequality of bigints

--- source/bigint.hpp
+++ source/bigint.hpp
@@ -2,6 +2,10 @@
 #define PK_BIGINT_HPP
 
 
+#include <algorithm>
+#include <cstring>
+
+
 namespace pk
 {
 
@@ -12,11 +16,37 @@ class bigint
 public:
     bigint() { s[0] = '0'; s[1] = '\0'; }
 
-    const char* c_str() { return s; }
+    bigint(const char* init_value)
+    {
+        const int size = strlen(init_value);
+        std::copy(init_value, init_value + size + 1, s);
+
+        if(s[0] == '-' && s[1] == '0')
+        {
+            s[0] = '0';
+            s[1] = '\0';
+        }
+    }
+
+    const char* c_str() const { return s; }
 
 private:
     char s[MAX_DECIMAL_SIZE];
 };
+
+
+template<int MAX_DECIMAL_SIZE>
+inline bool operator==(const bigint<MAX_DECIMAL_SIZE>& left, const bigint<MAX_DECIMAL_SIZE>& right)
+{
+    return (strcmp(left.c_str(), right.c_str()) == 0);
+}
+
+
+template<int MAX_DECIMAL_SIZE>
+inline bool operator!=(const bigint<MAX_DECIMAL_SIZE>& left, const bigint<MAX_DECIMAL_SIZE>& right)
+{
+    return !(left == right);
+}
 
 
 } // namespace pk
--- tests/bigint_tester.cpp
+++ tests/bigint_tester.cpp
@@ -16,7 +16,12 @@ namespace testing
 
 struct bigint_tester : public gt::Test
 {
-    static const int MAX_DECIMAL_SIZE = 5;
+    static const int MAX_DECIMAL_SIZE = 100;
+
+    pk::bigint<MAX_DECIMAL_SIZE> bi(const char* init_value)
+    {
+        return pk::bigint<MAX_DECIMAL_SIZE>(init_value);
+    }
 };
 
 
@@ -27,6 +32,43 @@ TEST_F(bigint_tester, tests_default_value)
 
     // when and then
     ASSERT_EQ(0, strcmp(a.c_str(), "0"));
+}
+
+
+TEST_F(bigint_tester, tests_initialization_from_cstring)
+{
+    // given
+    pk::bigint<MAX_DECIMAL_SIZE> a("0");
+    pk::bigint<MAX_DECIMAL_SIZE> b("1");
+    pk::bigint<MAX_DECIMAL_SIZE> c("12345");
+    pk::bigint<MAX_DECIMAL_SIZE> d("-999");
+    pk::bigint<MAX_DECIMAL_SIZE> e("-0");
+
+    // when and then
+    ASSERT_EQ(0, strcmp(a.c_str(), "0"));
+    ASSERT_EQ(0, strcmp(b.c_str(), "1"));
+    ASSERT_EQ(0, strcmp(c.c_str(), "12345"));
+    ASSERT_EQ(0, strcmp(d.c_str(), "-999"));
+    ASSERT_EQ(0, strcmp(e.c_str(), "0"));
+}
+
+
+TEST_F(bigint_tester, tests_equality)
+{
+    // when and then
+    ASSERT_EQ(bi("0"), bi("0"));
+    ASSERT_EQ(bi("-0"), bi("0"));
+    ASSERT_EQ(bi("123"), bi("123"));
+    ASSERT_EQ(bi("-456"), bi("-456"));
+}
+
+
+TEST_F(bigint_tester, tests_inequality)
+{
+    // when and then
+    ASSERT_NE(bi("0"), bi("1"));
+    ASSERT_NE(bi("123"), bi("456"));
+    ASSERT_NE(bi("-1"), bi("1"));
 }
 
 
