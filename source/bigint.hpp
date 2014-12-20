d1a99f4cf08cc247eff8df6cef529b0fe87ace2b
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2014-12-20 17:23:13 +0100

    More functional bigint

--- source/bigint.hpp
+++ source/bigint.hpp
@@ -8,30 +8,173 @@
 
 namespace pk
 {
+namespace detail
+{
+
+
+typedef unsigned char digit_type;
+
+
+inline digit_type char_to_digit(const char c)
+{
+    return (c - '0');
+}
+
+
+inline digit_type digit_to_char(const digit_type d)
+{
+    return ('0' + d);
+}
+
+
+} // namespace detail
 
 
 template<int MAX_DECIMAL_SIZE>
 class bigint
 {
 public:
-    bigint() { s[0] = '0'; s[1] = '\0'; }
+    bigint() : sz(1), sgn(sign_plus) { digits[0] = 0; }
+
+    bigint(const bigint& other) : sz(other.sz), sgn(other.sgn)
+    {
+        std::copy(other.digits, other.digits + sz, digits);
+    }
+
+    bigint& operator=(const bigint& other)
+    {
+        sz = other.sz;
+        sgn = other.sgn;
+
+        std::copy(other.digits, other.digits + sz, digits);
+
+        return *this;
+    }
 
     bigint(const char* init_value)
     {
-        const int size = strlen(init_value);
-        std::copy(init_value, init_value + size + 1, s);
-
-        if(s[0] == '-' && s[1] == '0')
+        if(init_value[0] == '-')
         {
-            s[0] = '0';
-            s[1] = '\0';
+            sgn = sign_minus;
+            initialize_digits(init_value + 1);
+
+            check_minus_zero();
+        }
+        else
+        {
+            sgn = sign_plus;
+            initialize_digits(init_value);
         }
     }
 
-    const char* c_str() const { return s; }
+    bigint& operator+()
+    {
+        return *this;
+    }
+
+    const bigint& operator+() const
+    {
+        return *this;
+    }
+
+    bigint operator-()
+    {
+        sgn = opposite_sign(sgn);
+        bigint result = *this;
+        sgn = opposite_sign(sgn);
+
+        return result;
+    }
+
+    bool is_positive() const
+    {
+        return ((sgn == sign_plus) && !(is_zero()));
+    }
+
+    bool is_negative() const
+    {
+        return (sgn == sign_minus);
+    }
+
+    const char* c_str() const
+    {
+        if(sgn == sign_minus)
+        {
+            cstr[0] = '-';
+            std::transform(digits, digits + sz, cstr + 1, detail::digit_to_char);
+            std::reverse(cstr + 1, cstr + sz + 1);
+            cstr[sz+1] = '\0';
+        }
+        else
+        {
+            std::transform(digits, digits + sz, cstr, detail::digit_to_char);
+            std::reverse(cstr, cstr + sz);
+            cstr[sz] = '\0';
+        }
+
+        return cstr;
+    }
 
 private:
-    char s[MAX_DECIMAL_SIZE];
+    enum sign
+    {
+        sign_plus = 0,
+        sign_minus = 1
+    };
+
+    sign opposite_sign(const sign s)
+    {
+        return (1 - s);
+    }
+
+    void initialize_digits(const char* init_value)
+    {
+        sz = strlen(init_value);
+
+        std::transform(init_value, init_value + sz, digits, detail::char_to_digit);
+        std::reverse(digits, digits + sz);
+    }
+
+    void remove_leading_zeros()
+    {
+        while((sz > 1) && (digits[sz-1] == 0))
+            --sz;
+    }
+
+    void check_minus_zero()
+    {
+        if(sgn == sign_plus)
+            return;
+
+        if(sz > 1)
+            return;
+
+        if(digits[0] == 0)
+            sgn = sign_plus;
+    }
+
+    bool is_zero() const
+    {
+        if((sz == 1) && (digits[0] == 0))
+            return true;
+
+        return false;
+    }
+
+    detail::digit_type digit_or_zero(const int i) const
+    {
+        if(i < sz)
+            return digits[i];
+
+        return 0;
+    }
+
+    detail::digit_type digits[MAX_DECIMAL_SIZE];
+    int sz;
+
+    sign sgn;
+
+    mutable char cstr[MAX_DECIMAL_SIZE];
 };
 
 
@@ -46,6 +189,76 @@ template<int MAX_DECIMAL_SIZE>
 inline bool operator!=(const bigint<MAX_DECIMAL_SIZE>& left, const bigint<MAX_DECIMAL_SIZE>& right)
 {
     return !(left == right);
+}
+
+
+template<int MAX_DECIMAL_SIZE>
+inline bool operator<(const bigint<MAX_DECIMAL_SIZE>& left, const bigint<MAX_DECIMAL_SIZE>& right)
+{
+    const char* left_rep = left.c_str();
+    const char* right_rep = right.c_str();
+
+    if(left_rep[0] == '-')
+    {
+        if(right_rep[0] == '-')
+        {
+            ++left_rep;
+            ++right_rep;
+
+            const int left_size = strlen(left_rep);
+            const int right_size = strlen(right_rep);
+
+            if(left_size < right_size)
+                return false;
+
+            if(left_size > right_size)
+                return true;
+
+            for(int i = 0; i < left_size; ++i)
+            {
+                if(left_rep[i] < right_rep[i])
+                    return false;
+
+                if(left_rep[i] > right_rep[i])
+                    return true;
+            }
+
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    else
+    {
+        if(right_rep[0] == '-')
+        {
+            return false;
+        }
+        else
+        {
+            const int left_size = strlen(left_rep);
+            const int right_size = strlen(right_rep);
+
+            if(left_size < right_size)
+                return true;
+
+            if(left_size > right_size)
+                return false;
+
+            for(int i = 0; i < left_size; ++i)
+            {
+                if(left_rep[i] < right_rep[i])
+                    return true;
+
+                if(left_rep[i] > right_rep[i])
+                    return false;
+            }
+
+            return false;
+        }
+    }
 }
 
 
--- tests/bigint_tester.cpp
+++ tests/bigint_tester.cpp
@@ -31,7 +31,7 @@ TEST_F(bigint_tester, tests_default_value)
     pk::bigint<MAX_DECIMAL_SIZE> a;
 
     // when and then
-    ASSERT_EQ(0, strcmp(a.c_str(), "0"));
+    EXPECT_EQ(0, strcmp(a.c_str(), "0"));
 }
 
 
@@ -45,30 +45,80 @@ TEST_F(bigint_tester, tests_initialization_from_cstring)
     pk::bigint<MAX_DECIMAL_SIZE> e("-0");
 
     // when and then
-    ASSERT_EQ(0, strcmp(a.c_str(), "0"));
-    ASSERT_EQ(0, strcmp(b.c_str(), "1"));
-    ASSERT_EQ(0, strcmp(c.c_str(), "12345"));
-    ASSERT_EQ(0, strcmp(d.c_str(), "-999"));
-    ASSERT_EQ(0, strcmp(e.c_str(), "0"));
+    EXPECT_EQ(0, strcmp(a.c_str(), "0"));
+    EXPECT_EQ(0, strcmp(b.c_str(), "1"));
+    EXPECT_EQ(0, strcmp(c.c_str(), "12345"));
+    EXPECT_EQ(0, strcmp(d.c_str(), "-999"));
+    EXPECT_EQ(0, strcmp(e.c_str(), "0"));
+}
+
+
+TEST_F(bigint_tester, tests_is_positive)
+{
+    EXPECT_TRUE(bi("42").is_positive());
+    EXPECT_TRUE(bi("1337").is_positive());
+
+    EXPECT_FALSE(bi("0").is_positive());
+    EXPECT_FALSE(bi("-42").is_positive());
+    EXPECT_FALSE(bi("-1337").is_positive());
+}
+
+
+TEST_F(bigint_tester, tests_is_negative)
+{
+    EXPECT_FALSE(bi("0").is_negative());
+    EXPECT_FALSE(bi("42").is_negative());
+    EXPECT_FALSE(bi("1337").is_negative());
+
+    EXPECT_TRUE(bi("-42").is_negative());
+    EXPECT_TRUE(bi("-1337").is_negative());
 }
 
 
 TEST_F(bigint_tester, tests_equality)
 {
     // when and then
-    ASSERT_EQ(bi("0"), bi("0"));
-    ASSERT_EQ(bi("-0"), bi("0"));
-    ASSERT_EQ(bi("123"), bi("123"));
-    ASSERT_EQ(bi("-456"), bi("-456"));
+    EXPECT_TRUE(bi("0") == bi("0"));
+    EXPECT_TRUE(bi("-0") == bi("0"));
+    EXPECT_TRUE(bi("123") == bi("123"));
+    EXPECT_TRUE(bi("-456") == bi("-456"));
+
+    EXPECT_FALSE(bi("0") == bi("1"));
+    EXPECT_FALSE(bi("123") == bi("456"));
+    EXPECT_FALSE(bi("-1") == bi("1"));
+    EXPECT_FALSE(bi("-999") == bi("-888"));
 }
 
 
 TEST_F(bigint_tester, tests_inequality)
 {
     // when and then
-    ASSERT_NE(bi("0"), bi("1"));
-    ASSERT_NE(bi("123"), bi("456"));
-    ASSERT_NE(bi("-1"), bi("1"));
+    EXPECT_TRUE(bi("0") != bi("1"));
+    EXPECT_TRUE(bi("123") != bi("456"));
+    EXPECT_TRUE(bi("-1") != bi("1"));
+    EXPECT_TRUE(bi("-999") != bi("-888"));
+
+    EXPECT_FALSE(bi("0") != bi("0"));
+    EXPECT_FALSE(bi("-0") != bi("0"));
+    EXPECT_FALSE(bi("123") != bi("123"));
+    EXPECT_FALSE(bi("-456") != bi("-456"));
+}
+
+
+TEST_F(bigint_tester, tests_lower)
+{
+    // when and then
+    EXPECT_TRUE(bi("0") < bi("1"));
+    EXPECT_TRUE(bi("123") < bi("456"));
+    EXPECT_TRUE(bi("-1") < bi("0"));
+    EXPECT_TRUE(bi("-42") < bi("1337"));
+
+    EXPECT_FALSE(bi("0") < bi("0"));
+    EXPECT_FALSE(bi("123") < bi("123"));
+    EXPECT_FALSE(bi("1337") < bi("42"));
+    EXPECT_FALSE(bi("-42") < bi("-42"));
+    EXPECT_FALSE(bi("-42") < bi("-1337"));
+    EXPECT_FALSE(bi("42") < bi("-1337"));
 }
 
 
