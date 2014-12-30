a2f51a495b380e5a6036b2bc044a7c003a82e1a6
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2014-12-30 13:20:39 +0100

    Strictly decreasing longest monotonic subsequence ready

--- source/longest_monotonic_subsequence.hpp
+++ source/longest_monotonic_subsequence.hpp
@@ -3,6 +3,7 @@
 
 
 #include <algorithm>
+#include <functional>
 #include <iterator>
 #include <limits>
 
@@ -36,6 +37,32 @@ public:
 
         int longest = 0;
         while(layers[longest] != infinity)
+            ++longest;
+
+        return (longest - 1);
+    }
+
+    template<class ForwardIterator>
+    static int strictly_decreasing(ForwardIterator first, ForwardIterator last)
+    {
+        typedef typename std::iterator_traits<ForwardIterator>::value_type value_type;
+        const value_type minus_infinity = std::numeric_limits<value_type>::min();
+        const int sequence_size = std::distance(first, last);
+        const int layers_size = sequence_size + 2;
+
+        value_type layers[MAX_SEQUENCE_SIZE+2];
+
+        std::fill(layers, layers + layers_size, minus_infinity);
+        layers[0] = std::numeric_limits<value_type>::max();
+
+        while(first != last)
+        {
+            value_type* t = std::lower_bound(layers, layers + layers_size, *first, std::greater<value_type>());
+            *t = *first++;
+        }
+
+        int longest = 0;
+        while(layers[longest] != minus_infinity)
             ++longest;
 
         return (longest - 1);
