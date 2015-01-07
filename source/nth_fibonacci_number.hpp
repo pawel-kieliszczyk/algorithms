4a0ef504907d5ff369603bfaa2e07519d762d83b
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2015-01-07 17:36:33 +0100

    Calculating nth Fibonacci number modulo some value

--- source/nth_fibonacci_number.hpp
+++ source/nth_fibonacci_number.hpp
@@ -53,6 +53,51 @@ int nth_fibonacci_number(int n)
 }
 
 
+template<class T>
+T nth_fibonacci_number(int n, const T& mod)
+{
+    if(n == 0)
+        return T(0);
+
+    T m_prev[2][2] = {{T(1), T(0)}, {T(0), T(1)}};
+    T m_next[2][2];
+
+    T (*m_prev_p)[2] = m_prev;
+    T (*m_next_p)[2] = m_next;
+
+    T c_next[2][2] = {{T(1), T(1)}, {T(1), T(0)}};
+    T c_prev[2][2];
+
+    T (*c_next_p)[2] = c_next;
+    T (*c_prev_p)[2] = c_prev;
+
+    --n;
+    while(n > 0)
+    {
+        if(n % T(2) == T(1))
+        {
+            m_next_p[0][0] = (m_prev_p[0][0] * c_next_p[0][0] + m_prev_p[0][1] * c_next_p[1][0]) % mod;
+            m_next_p[0][1] = (m_prev_p[0][0] * c_next_p[0][1] + m_prev_p[0][1] * c_next_p[1][1]) % mod;
+            m_next_p[1][0] = (m_prev_p[1][0] * c_next_p[0][0] + m_prev_p[1][1] * c_next_p[1][0]) % mod;
+            m_next_p[1][1] = (m_prev_p[1][0] * c_next_p[0][1] + m_prev_p[1][1] * c_next_p[1][1]) % mod;
+
+            std::swap(m_prev_p, m_next_p);
+        }
+
+        c_prev_p[0][0] = (c_next_p[0][0] * c_next_p[0][0] + c_next_p[0][1] * c_next_p[1][0]) % mod;
+        c_prev_p[0][1] = (c_next_p[0][0] * c_next_p[0][1] + c_next_p[0][1] * c_next_p[1][1]) % mod;
+        c_prev_p[1][0] = (c_next_p[1][0] * c_next_p[0][0] + c_next_p[1][1] * c_next_p[1][0]) % mod;
+        c_prev_p[1][1] = (c_next_p[1][0] * c_next_p[0][1] + c_next_p[1][1] * c_next_p[1][1]) % mod;
+
+        std::swap(c_prev_p, c_next_p);
+
+        n /= T(2);
+    }
+
+    return m_prev_p[0][0];
+}
+
+
 } // namespace pk
 
 
--- tests/nth_fibonacci_number_tester.cpp
+++ tests/nth_fibonacci_number_tester.cpp
@@ -19,6 +19,7 @@ struct nth_fibonacci_number_tester : public gt::Test
 
 TEST_F(nth_fibonacci_number_tester, tests_fibonacci_numbers)
 {
+    // when and then
     EXPECT_EQ(0, nth_fibonacci_number(0));
     EXPECT_EQ(1, nth_fibonacci_number(1));
     EXPECT_EQ(1, nth_fibonacci_number(2));
@@ -29,6 +30,29 @@ TEST_F(nth_fibonacci_number_tester, tests_fibonacci_numbers)
     EXPECT_EQ(13, nth_fibonacci_number(7));
     EXPECT_EQ(21, nth_fibonacci_number(8));
     EXPECT_EQ(34, nth_fibonacci_number(9));
+    EXPECT_EQ(55, nth_fibonacci_number(10));
+    EXPECT_EQ(89, nth_fibonacci_number(11));
+}
+
+
+TEST_F(nth_fibonacci_number_tester, tests_fibonacci_numbers_modulo)
+{
+    // given
+    const int mod = 4;
+
+    // when and then
+    EXPECT_EQ(0, nth_fibonacci_number(0, mod));
+    EXPECT_EQ(1, nth_fibonacci_number(1, mod));
+    EXPECT_EQ(1, nth_fibonacci_number(2, mod));
+    EXPECT_EQ(2, nth_fibonacci_number(3, mod));
+    EXPECT_EQ(3, nth_fibonacci_number(4, mod));
+    EXPECT_EQ(1, nth_fibonacci_number(5, mod));
+    EXPECT_EQ(0, nth_fibonacci_number(6, mod));
+    EXPECT_EQ(1, nth_fibonacci_number(7, mod));
+    EXPECT_EQ(1, nth_fibonacci_number(8, mod));
+    EXPECT_EQ(2, nth_fibonacci_number(9, mod));
+    EXPECT_EQ(3, nth_fibonacci_number(10, mod));
+    EXPECT_EQ(1, nth_fibonacci_number(11, mod));
 }
 
 
