5ed7b6b8c6a6f98b988c71d26195910261d4d18f
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2015-01-21 20:58:36 +0100

    Simplified gcd implementation

--- source/meta/greatest_common_divisor.hpp
+++ source/meta/greatest_common_divisor.hpp
@@ -6,36 +6,19 @@ namespace pk
 {
 namespace meta
 {
-namespace detail
-{
 
 
-template<bool BGreaterThanZero, int A, int B>
+template<int A, int B>
 struct greatest_common_divisor
+{
+    static const int value = greatest_common_divisor<B, A % B>::value;
+};
+
+
+template<int A>
+struct greatest_common_divisor<A, 0>
 {
     static const int value = A;
-};
-
-
-template<int A, int B>
-struct greatest_common_divisor<true, A, B>
-{
-private:
-    static const int AA = B;
-    static const int BB = A % B;
-
-public:
-    static const int value = greatest_common_divisor<(BB > 0), AA, BB>::value;
-};
-
-
-} // namespace detail
-
-
-template<int A, int B>
-struct greatest_common_divisor
-{
-    static const int value = detail::greatest_common_divisor<(B > 0), A, B>::value;
 };
 
 
