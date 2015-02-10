fc02aa85f3afd9b9ef1da7035d51193bd959f0cb
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2015-02-10 20:13:46 +0100

    Finding majority element in custom sequences

---
+++ tests/majority_element_tester.cpp
@@ -0,0 +1,175 @@
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "majority_element.hpp"
+
+
+namespace gt = testing;
+
+
+namespace pk
+{
+namespace testing
+{
+
+
+struct majority_element_tester : public gt::Test
+{
+    //
+};
+
+
+TEST_F(majority_element_tester, tests_existing_majority_element_in_one_element_sequence)
+{
+    // given
+    std::vector<int> numbers;
+    numbers.push_back(42);
+
+    // when
+    std::vector<int>::iterator elem = majority_element(numbers.begin(), numbers.end());
+
+    // then
+    ASSERT_NE(numbers.end(), elem);
+    EXPECT_EQ(42, *elem);
+}
+
+
+TEST_F(majority_element_tester, tests_nonexisting_majority_element_in_two_element_sequence)
+{
+    // given
+    std::vector<int> numbers;
+    numbers.push_back(11);
+    numbers.push_back(22);
+
+    // when
+    std::vector<int>::iterator elem = majority_element(numbers.begin(), numbers.end());
+
+    // then
+    ASSERT_EQ(numbers.end(), elem);
+}
+
+
+TEST_F(majority_element_tester, tests_existing_majority_element_in_two_element_sequence)
+{
+    // given
+    std::vector<int> numbers;
+    numbers.push_back(11);
+    numbers.push_back(11);
+
+    // when
+    std::vector<int>::iterator elem = majority_element(numbers.begin(), numbers.end());
+
+    // then
+    ASSERT_NE(numbers.end(), elem);
+    ASSERT_EQ(11, *elem);
+}
+
+
+TEST_F(majority_element_tester, tests_nonexisting_majority_element_in_three_element_sequence)
+{
+    // given
+    std::vector<int> numbers;
+    numbers.push_back(11);
+    numbers.push_back(22);
+    numbers.push_back(33);
+
+    // when
+    std::vector<int>::iterator elem = majority_element(numbers.begin(), numbers.end());
+
+    // then
+    ASSERT_EQ(numbers.end(), elem);
+}
+
+
+TEST_F(majority_element_tester, tests_existing_majority_element_in_three_element_sequence)
+{
+    // given
+    std::vector<int> numbers;
+    numbers.push_back(11);
+    numbers.push_back(22);
+    numbers.push_back(22);
+
+    // when
+    std::vector<int>::iterator elem = majority_element(numbers.begin(), numbers.end());
+
+    // then
+    ASSERT_NE(numbers.end(), elem);
+    ASSERT_EQ(22, *elem);
+}
+
+
+TEST_F(majority_element_tester, tests_nonexisting_majority_element_in_four_element_sequence)
+{
+    // given
+    std::vector<int> numbers;
+    numbers.push_back(11);
+    numbers.push_back(11);
+    numbers.push_back(22);
+    numbers.push_back(22);
+
+    // when
+    std::vector<int>::iterator elem = majority_element(numbers.begin(), numbers.end());
+
+    // then
+    ASSERT_EQ(numbers.end(), elem);
+}
+
+
+TEST_F(majority_element_tester, tests_existing_majority_element_in_four_element_sequence)
+{
+    // given
+    std::vector<int> numbers;
+    numbers.push_back(42);
+    numbers.push_back(42);
+    numbers.push_back(42);
+    numbers.push_back(42);
+
+    // when
+    std::vector<int>::iterator elem = majority_element(numbers.begin(), numbers.end());
+
+    // then
+    ASSERT_NE(numbers.end(), elem);
+    ASSERT_EQ(42, *elem);
+}
+
+
+TEST_F(majority_element_tester, tests_nonexisting_majority_element_in_five_element_sequence)
+{
+    // given
+    std::vector<int> numbers;
+    numbers.push_back(11);
+    numbers.push_back(11);
+    numbers.push_back(22);
+    numbers.push_back(22);
+    numbers.push_back(33);
+
+    // when
+    std::vector<int>::iterator elem = majority_element(numbers.begin(), numbers.end());
+
+    // then
+    ASSERT_EQ(numbers.end(), elem);
+}
+
+
+TEST_F(majority_element_tester, tests_existing_majority_element_in_five_element_sequence)
+{
+    // given
+    std::vector<int> numbers;
+    numbers.push_back(11);
+    numbers.push_back(11);
+    numbers.push_back(22);
+    numbers.push_back(22);
+    numbers.push_back(11);
+
+    // when
+    std::vector<int>::iterator elem = majority_element(numbers.begin(), numbers.end());
+
+    // then
+    ASSERT_NE(numbers.end(), elem);
+    ASSERT_EQ(11, *elem);
+}
+
+
+} // namespace testing
+} // namespace pk
