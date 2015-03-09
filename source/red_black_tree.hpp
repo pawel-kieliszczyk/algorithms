ec1826b175c5f8eaf4490d47b5219f0922f806f0
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2015-03-09 17:49:13 +0100

    Red-black tree with basic functionality

--- source/red_black_tree.hpp
+++ source/red_black_tree.hpp
@@ -2,21 +2,55 @@
 #define PK_REDBLACKTREE_HPP
 
 
+#include "allocator.hpp"
+
+
 namespace pk
 {
 
 
-template<class T, int MaxSize>
+/**
+ * Red-black tree which keeps distinct elements (no duplications allowed).
+ */
+template<class T, int MaxElements, template<class, int> class Allocator = allocator>
 class red_black_tree
 {
 public:
     typedef T value_type;
 
-    red_black_tree() : sz(0) {}
+    red_black_tree() : root(&null_node), sz(0)
+    {
+        null_node.color = node::color_black;
+    }
 
     void insert(const value_type& elem)
     {
+        node* new_node = alloc.allocate();
+        new_node->value = elem;
+
+        insert(new_node);
+
         ++sz;
+    }
+
+    void remove(const value_type& elem)
+    {
+        node* z = node_with_value(elem);
+        remove(z);
+
+        alloc.deallocate(z);
+
+        --sz;
+    }
+
+    bool exists(const value_type& elem) const
+    {
+        node* result = root;
+
+        while((result != &null_node) && (result->value != elem))
+            result = (elem < result->value) ? result->left : result->right;
+
+        return (result != &null_node);
     }
 
     int size() const
@@ -30,6 +64,299 @@ public:
     }
 
 private:
+    struct node
+    {
+        enum ecolor { color_red, color_black };
+
+        value_type value;
+        ecolor color;
+
+        node* parent;
+        node* left;
+        node* right;
+    };
+
+    void insert(node* new_node)
+    {
+        node* y = &null_node;
+        node* x = root;
+
+        while(x != &null_node)
+        {
+            y = x;
+            x = (new_node->value < x->value) ? x->left : x->right;
+        }
+
+        new_node->parent = y;
+        if(y == &null_node)
+            root = new_node;
+        else if(new_node->value < y->value)
+            y->left = new_node;
+        else
+            y->right = new_node;
+
+        new_node->left = &null_node;
+        new_node->right = &null_node;
+        new_node->color = node::color_red;
+
+        fix_up_after_insert(new_node);
+    }
+
+    void remove(node* z)
+    {
+        node* y = z;
+        typename node::ecolor y_original_color = y->color;
+
+        node* x;
+        if(z->left == &null_node)
+        {
+            x = z->right;
+            transplant(z, z->right);
+        }
+        else if(z->right == &null_node)
+        {
+            x = z->left;
+            transplant(z, z->left);
+        }
+        else
+        {
+            y = node_in_subtree_with_minimum_value(z->right);
+            y_original_color = y->color;
+            x = y->right;
+
+            if(y->parent == z)
+            {
+                x->parent = y;
+            }
+            else
+            {
+                transplant(y, y->right);
+
+                y->right = z->right;
+                y->right->parent = y;
+            }
+
+            transplant(z, y);
+
+            y->left = z->left;
+            y->left->parent = y;
+            y->color = z->color;
+        }
+
+        if(y_original_color == node::color_black)
+            fix_up_after_remove(x);
+    }
+
+    void fix_up_after_insert(node* z)
+    {
+        while(z->parent->color == node::color_red)
+        {
+            if(z->parent == z->parent->parent->left)
+            {
+                node* y = z->parent->parent->right;
+                if(y->color == node::color_red)
+                {
+                    z->parent->color = node::color_black;
+                    y->color = node::color_black;
+                    z->parent->parent->color = node::color_red;
+                    z = z->parent->parent;
+                }
+                else
+                {
+                    if(z == z->parent->right)
+                    {
+                        z = z->parent;
+                        rotate_left(z);
+                    }
+
+                    z->parent->color = node::color_black;
+                    z->parent->parent->color = node::color_red;
+                    rotate_right(z->parent->parent);
+                }
+            }
+            else
+            {
+                node* y = z->parent->parent->left;
+                if(y && y->color == node::color_red)
+                {
+                    z->parent->color = node::color_black;
+                    y->color = node::color_black;
+                    z->parent->parent->color = node::color_red;
+                    z = z->parent->parent;
+                }
+                else
+                {
+                    if(z == z->parent->left)
+                    {
+                        z = z->parent;
+                        rotate_right(z);
+                    }
+
+                    z->parent->color = node::color_black;
+                    z->parent->parent->color = node::color_red;
+                    rotate_left(z->parent->parent);
+                }
+            }
+        }
+
+        root->color = node::color_black;
+    }
+
+    void fix_up_after_remove(node* x)
+    {
+        while((x != root) && (x->color == node::color_black))
+        {
+            if(x == x->parent->left)
+            {
+                node* w = x->parent->right;
+
+                if(w->color == node::color_red)
+                {
+                    w->color = node::color_black;
+                    x->parent->color = node::color_red;
+                    rotate_left(x->parent);
+                    w = x->parent->right;
+                }
+
+                if((w->left->color == node::color_black) && (w->right->color == node::color_black))
+                {
+                    w->color = node::color_red;
+                    x = x->parent;
+                }
+                else
+                {
+                    if(w->right->color == node::color_black)
+                    {
+                        w->left->color = node::color_black;
+                        w->color = node::color_red;
+                        rotate_right(w);
+                        w = x->parent->right;
+                    }
+
+                    w->color = x->parent->color;
+                    x->parent->color = node::color_black;
+                    w->right->color = node::color_black;
+                    rotate_left(x->parent);
+                    x = root;
+                }
+            }
+            else
+            {
+                node* w = x->parent->left;
+
+                if(w->color == node::color_red)
+                {
+                    w->color = node::color_black;
+                    x->parent->color = node::color_red;
+                    rotate_right(x->parent);
+                    w = x->parent->left;
+                }
+
+                if((w->right->color == node::color_black) && (w->left->color == node::color_black))
+                {
+                    w->color = node::color_red;
+                    x = x->parent;
+                }
+                else
+                {
+                    if(w->left->color == node::color_black)
+                    {
+                        w->right->color = node::color_black;
+                        w->color = node::color_red;
+                        rotate_left(w);
+                        w = x->parent->left;
+                    }
+
+                    w->color = x->parent->color;
+                    x->parent->color = node::color_black;
+                    w->left->color = node::color_black;
+                    rotate_right(x->parent);
+                    x = root;
+                }
+            }
+        }
+
+        x->color = node::color_black;
+    }
+
+    void rotate_left(node* x)
+    {
+        node* y = x->right;
+        x->right = y->left;
+
+        if(y->left != &null_node)
+            y->left->parent = x;
+
+        y->parent = x->parent;
+
+        if(x->parent == &null_node)
+            root = y;
+        else if(x == x->parent->left)
+            x->parent->left = y;
+        else
+            x->parent->right = y;
+
+        y->left = x;
+        x->parent = y;
+    }
+
+    void rotate_right(node* x)
+    {
+        node* y = x->left;
+        x->left = y->right;
+
+        if(y->right != &null_node)
+            y->right->parent = x;
+
+        y->parent = x->parent;
+
+        if(x->parent == &null_node)
+            root = y;
+        else if(x == x->parent->right)
+            x->parent->right = y;
+        else
+            x->parent->left = y;
+
+        y->right = x;
+        x->parent = y;
+    }
+
+    node* node_with_value(const value_type& value)
+    {
+        node* result = root;
+
+        while(result->value != value)
+            result = (value < result->value) ? result->left : result->right;
+
+        return result;
+    }
+
+
+    void transplant(node* u, node* v)
+    {
+        if(u->parent == &null_node)
+            root = v;
+        else if(u == u->parent->left)
+            u->parent->left = v;
+        else
+            u->parent->right = v;
+
+        v->parent = u->parent;
+    }
+
+    node* node_in_subtree_with_minimum_value(node* x)
+    {
+        while(x->left != &null_node)
+            x = x->left;
+
+        return x;
+    }
+
+    Allocator<node, MaxElements> alloc;
+
+    node* root;
+    node null_node;
+
     int sz;
 };
 
--- tests/red_black_tree_tester.cpp
+++ tests/red_black_tree_tester.cpp
@@ -14,7 +14,7 @@ namespace testing
 
 struct red_black_tree_tester : public gt::Test
 {
-    static const int MAX_SIZE = 20;
+    static const int MAX_SIZE = 100;
 
     // tested class:
     red_black_tree<int, MAX_SIZE> rbt;
@@ -28,7 +28,7 @@ TEST_F(red_black_tree_tester, tests_emptyness_of_empty_tree)
 }
 
 
-TEST_F(red_black_tree_tester, tests_emptyness_of_non_empty_tree)
+TEST_F(red_black_tree_tester, tests_emptyness_after_insertion)
 {
     // given
     const int elem = 11;
@@ -41,14 +41,21 @@ TEST_F(red_black_tree_tester, tests_emptyness_of_non_empty_tree)
 }
 
 
-TEST_F(red_black_tree_tester, tests_size_of_empty_tree)
+TEST_F(red_black_tree_tester, tests_emptyness_after_insertion_and_removal)
 {
-    // when and then
-    EXPECT_EQ(0, rbt.size());
+    // given
+    const int elem = 11;
+    rbt.insert(elem);
+
+    // when
+    rbt.remove(elem);
+
+    // then
+    EXPECT_TRUE(rbt.empty());
 }
 
 
-TEST_F(red_black_tree_tester, tests_size_of_non_empty_tree)
+TEST_F(red_black_tree_tester, tests_emptyness_after_mixed_insertions_and_removals)
 {
     // given
     const int elem1 = 11;
@@ -58,10 +65,160 @@ TEST_F(red_black_tree_tester, tests_size_of_non_empty_tree)
     // when
     rbt.insert(elem1);
     rbt.insert(elem2);
+    rbt.remove(elem2);
+
+    // then
+    EXPECT_FALSE(rbt.empty());
+
+    // when
     rbt.insert(elem3);
 
     // then
-    EXPECT_EQ(3, rbt.size());
+    EXPECT_FALSE(rbt.empty());
+
+    // when
+    rbt.remove(elem1);
+    rbt.remove(elem3);
+
+    // then
+    EXPECT_TRUE(rbt.empty());
+}
+
+
+TEST_F(red_black_tree_tester, tests_size_of_empty_tree)
+{
+    // when and then
+    EXPECT_EQ(0, rbt.size());
+}
+
+
+TEST_F(red_black_tree_tester, tests_size_after_insertion)
+{
+    // given
+    const int elem = 11;
+
+    // when
+    rbt.insert(elem);
+
+    // then
+    EXPECT_EQ(1, rbt.size());
+}
+
+
+TEST_F(red_black_tree_tester, tests_size_after_insertion_and_removal)
+{
+    // given
+    const int elem = 11;
+    rbt.insert(elem);
+
+    // when
+    rbt.remove(elem);
+
+    // then
+    EXPECT_EQ(0, rbt.size());
+}
+
+
+TEST_F(red_black_tree_tester, tests_size_after_mixed_insertions_and_removals)
+{
+    // given
+    const int elem1 = 11;
+    const int elem2 = 22;
+    const int elem3 = 33;
+
+    // when
+    rbt.insert(elem1);
+    rbt.insert(elem2);
+    rbt.remove(elem2);
+
+    // then
+    EXPECT_EQ(1, rbt.size());
+
+    // when
+    rbt.insert(elem3);
+
+    // then
+    EXPECT_EQ(2, rbt.size());
+
+    // when
+    rbt.remove(elem1);
+    rbt.remove(elem3);
+
+    // then
+    EXPECT_EQ(0, rbt.size());
+}
+
+
+TEST_F(red_black_tree_tester, tests_existance_of_missing_element)
+{
+    // given
+    const int elem = 11;
+
+    // when and then
+    EXPECT_FALSE(rbt.exists(elem));
+}
+
+
+TEST_F(red_black_tree_tester, tests_existance_of_inserted_element)
+{
+    // given
+    const int elem = 11;
+
+    // when
+    rbt.insert(elem);
+
+    // then
+    EXPECT_TRUE(rbt.exists(elem));
+}
+
+
+TEST_F(red_black_tree_tester, tests_existance_of_removed_element)
+{
+    // given
+    const int elem = 11;
+    rbt.insert(elem);
+
+    // when
+    rbt.remove(elem);
+
+    // then
+    EXPECT_FALSE(rbt.exists(elem));
+}
+
+
+TEST_F(red_black_tree_tester, tests_existance_of_mixed_inserted_and_removed_elements)
+{
+    // given
+    const int elem1 = 11;
+    const int elem2 = 22;
+    const int elem3 = 33;
+
+    // when
+    rbt.insert(elem1);
+    rbt.insert(elem2);
+    rbt.remove(elem2);
+
+    // then
+    EXPECT_TRUE(rbt.exists(elem1));
+    EXPECT_FALSE(rbt.exists(elem2));
+    EXPECT_FALSE(rbt.exists(elem3));
+
+    // when
+    rbt.insert(elem3);
+
+    // then
+    EXPECT_TRUE(rbt.exists(elem1));
+    EXPECT_FALSE(rbt.exists(elem2));
+    EXPECT_TRUE(rbt.exists(elem3));
+
+    // when
+    rbt.remove(elem1);
+    rbt.remove(elem3);
+
+    // then
+    EXPECT_FALSE(rbt.exists(elem1));
+    EXPECT_FALSE(rbt.exists(elem2));
+    EXPECT_FALSE(rbt.exists(elem3));
 }
 
 
