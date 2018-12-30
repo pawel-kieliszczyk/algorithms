e100d3907409206fe2133f098f4bdd5ad40dde1a
Pawel Kieliszczyk <pawel.kieliszczyk@gmail.com>
2018-12-30 13:43:06 +0000

    Manacher's algorithm for finding even palindromic substrings

--- source/palindromic_substrings_manacher.hpp
+++ source/palindromic_substrings_manacher.hpp
@@ -44,6 +44,32 @@ public:
            i += k;
         }
     }
+    /**
+     * WARNING: text_size parameter should count "guards" as well
+     */
+    void find_odd(const char* text, const int text_size)
+    {
+        palindromic_substrings_table[0] = 0;
+        int i = 1;
+        int t = 1;
+        while(i < text_size)
+        {
+           while(text[i-t] == text[i+t])
+               ++t;
+
+           palindromic_substrings_table[i] = t;
+
+           int k = 1;
+           while((k <= t) && (palindromic_substrings_table[i-k] != palindromic_substrings_table[i]-k))
+           {
+               palindromic_substrings_table[i+k] = std::min(palindromic_substrings_table[i-k], palindromic_substrings_table[i]-k);
+              ++k;
+           }
+
+           t = std::max(1, t-k);
+           i += k;
+        }
+    }
 
     /**
      * table[i] = k means that substring text[i-k+1,..,i+k] is a palindrome (of length 2*k)
